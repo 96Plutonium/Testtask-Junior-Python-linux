4cf5dda51e1f48ef0488ffbce2f410a72c7446c0
user <user@laptop>
2022-10-12 12:55:25 +0300

    ok basic workin

--- lib.py
+++ lib.py
@@ -1,4 +1,16 @@
 import requests
 
-def get_data(link:str) -> dict:
-    return requests.get(link).json()
+def _get_data(link:str) -> dict:
+    return requests.get(link).json()["packages"]
+
+def _all_packages(arg:dict) -> list:
+    ret = []
+    for _ in arg:
+        ret.append(_["name"])
+    return ret
+"""
+Combines _get_data and _all_packages 
+"""
+def get_list(arg:str) -> list:
+    link = f"https://rdb.altlinux.org/api/export/branch_binary_packages/{arg}"
+    return _all_packages( _get_data ( link ) )
--- tool.py
+++ tool.py
@@ -1,8 +1,23 @@
-import lib
+from lib import *
 import sys
+import pprint
 
 def main():
     if len(sys.argv) == 1:
         print (f"{sys.argv[0]} <branch> <branch1> --first --second --versions")
+    # default arguments 
+    if "https" not in str (sys.argv):
+        branch1 = "sisyphus"
+        branch2 = "p10"
+    else:
+        branch1 = sys.argv[1]
+        branch2 = sys.argv[2]
+    dataset = ""
+    if "--first" in sys.argv:
+        dataset = set ( get_list(branch1) ) - set ( get_list(branch2 ) )
+    elif "second" in sys.argv:
+        dataset = set ( get_list(branch1) ) - set ( get_list(branch2 ) )
+    if dataset:
+        pprint.pprint(dataset, width = 20, compact = True)
 if __name__ == "__main__":
     main()
